3a6e57dc8a235262191355518cff6947772d7445
vosem <trefoil2@ukr.net>
2017-04-06 13:15:37 +0300

    added UA WordClock

--- Portfolio/WordClock/wordClock.js
+++ Portfolio/WordClock/wordClock.js
@@ -7,9 +7,9 @@
             h-=12;
         }
         $("span.hour").css({"color": "black", "text-shadow": "none"});
-        for(var i = 0; i < 12; i++){
+        for(var i = 0; i <= 12; i++){
             if(i === h){
-                $("div.hour span:nth-child("+i+")").css({"color": "rgb(2, 255, 238)", "text-shadow": "0 0 0.3em rgb(2, 255, 238)"});
+                $("div.hour span:nth-child("+i+")").css({"color": "rgb(2, 255, 238)", "text-shadow": "0 0 0.1em rgb(2, 255, 238)"});
             }
         }
         setTimeout("colorizeHours()", 1000);
@@ -22,29 +22,29 @@
         for(var i = 0; i < 11; i++){
             if(i === m){
                 var index = m + 1;
-                $("div.digits span:nth-child("+index+")").css({"color": "rgb(2, 255, 238)", "text-shadow": "0 0 0.3em rgb(2, 255, 238)"});
+                $("div.digits span:nth-child("+index+")").css({"color": "rgb(2, 255, 238)", "text-shadow": "0 0 0.2em rgb(2, 255, 238)"});
             }
         }
 
         for(var i = 11; i < 20; i++){
             if(i === m){
                 var index = i-10;
-                $("div.11_19 span:nth-child("+index+")").css({"color": "rgb(2, 255, 238)", "text-shadow": "0 0 0.3em rgb(2, 255, 238)"});
+                $("div.11_19 span:nth-child("+index+")").css({"color": "rgb(2, 255, 238)", "text-shadow": "0 0 0.2em rgb(2, 255, 238)"});
             }
         }
 
         for(var i = 20; i < 60; i++){
             if(m >= 20 && m < 30){
-                $("div.tens span:nth-child(1)").css({"color": "rgb(2, 255, 238)", "text-shadow": "0 0 0.3em rgb(2, 255, 238)"});
+                $("div.tens span:nth-child(1)").css({"color": "rgb(2, 255, 238)", "text-shadow": "0 0 0.2em rgb(2, 255, 238)"});
             }
             else if(m >= 30 && m < 40){
-                $("div.tens span:nth-child(2)").css({"color": "rgb(2, 255, 238)", "text-shadow": "0 0 0.3em rgb(2, 255, 238)"});
+                $("div.tens span:nth-child(2)").css({"color": "rgb(2, 255, 238)", "text-shadow": "0 0 0.2em rgb(2, 255, 238)"});
             }
             else if(m >= 40 && m < 50){
-                $("div.tens span:nth-child(3)").css({"color": "rgb(2, 255, 238)", "text-shadow": "0 0 0.3em rgb(2, 255, 238)"});
+                $("div.tens span:nth-child(3)").css({"color": "rgb(2, 255, 238)", "text-shadow": "0 0 0.2em rgb(2, 255, 238)"});
             }
             else if(m >= 50 && m < 60){
-                $("div.tens span:nth-child(4)").css({"color": "rgb(2, 255, 238)", "text-shadow": "0 0 0.3em rgb(2, 255, 238)"});
+                $("div.tens span:nth-child(4)").css({"color": "rgb(2, 255, 238)", "text-shadow": "0 0 0.2em rgb(2, 255, 238)"});
             }
         }
         
@@ -58,7 +58,7 @@
                 continue;
             }
             var index = lastDigit+1;
-                $("div.digits span:nth-child("+index+")").css({"color": "rgb(2, 255, 238)", "text-shadow": "0 0 0.3em rgb(2, 255, 238)"});
+                $("div.digits span:nth-child("+index+")").css({"color": "rgb(2, 255, 238)", "text-shadow": "0 0 0.2em rgb(2, 255, 238)"});
             }
         }
         
@@ -70,10 +70,10 @@
         var h = now.getHours();
         if (h <= 12){
             $("div.am_pm span:nth-child(2)").css({"color": "black", "text-shadow": "none"});
-            $("div.am_pm span:nth-child(1)").css({"color": "rgb(2, 255, 238)", "text-shadow": "0 0 0.3em rgb(2, 255, 238)"});
+            $("div.am_pm span:nth-child(1)").css({"color": "rgb(2, 255, 238)", "text-shadow": "0 0 0.2em rgb(2, 255, 238)"});
         } else{
             $("div.am_pm span:nth-child(1)").css({"color": "black", "text-shadow": "none"});
-            $("div.am_pm span:nth-child(2)").css({"color": "rgb(2, 255, 238)", "text-shadow": "0 0 0.3em rgb(2, 255, 238)"});
+            $("div.am_pm span:nth-child(2)").css({"color": "rgb(2, 255, 238)", "text-shadow": "0 0 0.2em rgb(2, 255, 238)"});
         }
         setTimeout("colorizeAmPm()", 1000);
     };
