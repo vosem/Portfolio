1f16781a13ee0805e719bc40f7989415a02fb532
vosem <trefoil2@ukr.net>
2017-04-17 14:51:28 +0300

    adding adaptability

--- Portfolio/Showcase/showcase.js
+++ Portfolio/Showcase/showcase.js
@@ -1,52 +1,30 @@
 (function () {
 
-    // for(var i=0; i<=document.getElementsByClassName('button1').length; i++){
-    //     document.getElementsByClassName('button1')[i].onclick = function() {
-    //         document.getElementsByClassName('overlay-preview_photo')[i].style.display = 'block';
-    //     };
-    // }
+    var enlargeButtonArr = document.getElementsByClassName('enlarge_button');
+    var overlayPreviewPhotoArr = document.getElementsByClassName('overlay-preview_photo');
+    var closeButtonArr = document.getElementsByClassName('close_button');
 
-
-    document.getElementsByClassName('button1')[0].onclick = function() {
-        document.getElementsByClassName('overlay-preview_photo1')[0].style.display = 'block';
-    };
-    document.getElementsByClassName('close_button')[0].onclick = function () {
-        document.getElementsByClassName('overlay-preview_photo1')[0].style.display = 'none';
-    };
-
-    document.getElementsByClassName('button1')[1].onclick = function() {
-        document.getElementsByClassName('overlay-preview_photo2')[0].style.display = 'block';
-    };
-    document.getElementsByClassName('close_button')[1].onclick = function () {
-        document.getElementsByClassName('overlay-preview_photo2')[0].style.display = 'none';
-    };
-
-    document.getElementsByClassName('button1')[2].onclick = function() {
-        document.getElementsByClassName('overlay-preview_photo3')[0].style.display = 'block';
-    };
-    document.getElementsByClassName('close_button')[2].onclick = function () {
-        document.getElementsByClassName('overlay-preview_photo3')[0].style.display = 'none';
-    };
-
-    document.getElementsByClassName('button1')[3].onclick = function() {
-        document.getElementsByClassName('overlay-preview_photo4')[0].style.display = 'block';
-    };
-    document.getElementsByClassName('close_button')[3].onclick = function () {
-        document.getElementsByClassName('overlay-preview_photo4')[0].style.display = 'none';
-    };
-
-    document.getElementsByClassName('button1')[4].onclick = function() {
-        document.getElementsByClassName('overlay-preview_photo5')[0].style.display = 'block';
-    };
-    document.getElementsByClassName('close_button')[4].onclick = function () {
-        document.getElementsByClassName('overlay-preview_photo5')[0].style.display = 'none';
-    };
-
-    document.getElementsByClassName('button1')[5].onclick = function() {
-        document.getElementsByClassName('overlay-preview_photo6')[0].style.display = 'block';
-    };
-    document.getElementsByClassName('close_button')[5].onclick = function () {
-        document.getElementsByClassName('overlay-preview_photo6')[0].style.display = 'none';
+    for (var i = 0; i < enlargeButtonArr.length; i++) {
+        
+        enlargeButtonArr[i].onclick = function () {
+            var index = [].indexOf.call(enlargeButtonArr, this);
+            for (var j = 0; j <= overlayPreviewPhotoArr.length; j++) {
+                overlayPreviewPhotoArr[index].style.display = 'block';
+//                var elem = document.querySelectorAll('')
+//                console.log()
+            }
+        };
+        
+        closeButtonArr[i].onclick = function () {
+            var index = [].indexOf.call(closeButtonArr, this);
+            for (var j = 0; j <= overlayPreviewPhotoArr.length; j++) {
+                if (overlayPreviewPhotoArr[j] === overlayPreviewPhotoArr[index]) {
+                    overlayPreviewPhotoArr[index].style.display = 'none';
+                }
+            }
+        };
     }
-
+    
+   
+    
 })();
